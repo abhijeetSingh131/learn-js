d811b31b5ee22ea103244cda131bcfb439b40f43
abhijeetSingh131 <82701392+abhijeetSingh131@users.noreply.github.com>
2024-11-16 08:17:06 +0000

    conversion changes

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,36 @@
+// let score = "33a"
+// let score = null 
+let score = undefined 
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueinscore = Number(score)
+// console.log(valueinscore);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0;
+
+
+let isLoggedIn = "abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abhi" => true
+
+let num= 33
+let strnum= String(num)
+
+console.log(typeof strnum)
+
+
+
+
+
+
