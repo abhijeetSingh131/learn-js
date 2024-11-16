2652f93851dfc7db6b0c35eb44ebcdd6a08c89f7
abhijeetSingh131 <82701392+abhijeetSingh131@users.noreply.github.com>
2024-11-16 10:05:48 +0000

    operations changes

--- 01_basics/03_typeconversion.js
+++ 01_basics/03_typeconversion.js
@@ -27,8 +27,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let num= 33
 let strnum= String(num)
 
-console.log(typeof strnum)
+// console.log(typeof strnum)
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  2 power 3
+// console.log(2/3);
+// console.log(2%3);
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
