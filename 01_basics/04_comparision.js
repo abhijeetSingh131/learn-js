fd5fe13f2a5ffc358c8af992cb18e3f05ab7be16
abhijeetSingh131 <82701392+abhijeetSingh131@users.noreply.github.com>
2024-11-16 10:56:01 +0000

    comparision changes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  // false
+console.log(null == 0); // false
+console.log(null >= 0); // true because >= or <= work diffrently than > = because it convert null into a number which comes 0 >=0 this condition true 
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check
+
+console.log("2" === 2);
